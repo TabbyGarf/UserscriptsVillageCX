32d7fd216ed417fbd0470ecdde2d00ca8c3e3621
TabbyGarf <87911854+TabbyGarf@users.noreply.github.com>
2025-03-09 02:47:01 +0100

    Create silenttyping-ence.user.js

---
+++ silenttyping-ence.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Silent Typing by Ence
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       Encephallus / Encendre
+// @match        https://village.cx/*
+// @run-at       document-start
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=village.cx
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+  
+      function hookMethod(obj, method, hookFn) {
+      const realMethod = obj[method]
+  
+      function stopHook() {
+          obj[method] = realMethod
+      }
+  
+      function hook(...args) {
+          return hookFn({ stopHook, method: realMethod }, this, args)
+      }
+  
+      obj[method] = hook
+  
+      return stopHook
+  }
+  
+  hookMethod(WebSocket.prototype, "send", (ctx, obj, args) => {
+      const msg = args[0]
+      try {
+          const data = JSON.parse(msg)
+          if (data.type === "typing") {
+              return
+          }
+      } catch(e) {}
+  
+      return ctx.method.apply(obj, args)
+  })
+
+})();
