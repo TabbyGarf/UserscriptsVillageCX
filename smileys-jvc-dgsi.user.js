ed51478a8286477a9476bd8769d5582202daaf9c
TabbyGarf <87911854+TabbyGarf@users.noreply.github.com>
2025-04-09 00:48:11 +0200

    Update smileys-jvc-dgsi.user.js

--- smileys-jvc-dgsi.user.js
+++ smileys-jvc-dgsi.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Smileys JVC pour V.CX
-// @version      1
+// @version      1.1.0
 // @description  :hapoelparty:
 // @author       DGSI
 // @grant        none
@@ -16,16 +16,16 @@
 const oestrogen = [
     // Exemple: ['', '<img src="">'],
     /* JVC */
-    [':)', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/326dda02-857a-41cd-b96b-10b670707d81.gif">'],
-    [':(', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a44b536e-2911-4fb3-a5a7-936772c03def.gif">'],
-    [':noel:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a4c82724-3ca7-471e-a1fe-1008213fd975.gif">'],
-    [':hap:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/0f68c004-092c-4aec-a006-b42252d21c16.gif">'],
-    [':coeur:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a7523aae-879c-4123-aa7d-ef8383e7a619.gif">'],
-    [':question:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a7523aae-879c-4123-aa7d-ef8383e7a619.gif">'],
-    [':ok:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/48a51100-3155-45ec-b0c7-46b0ed19aea3.gif">'],
-    [':rire:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/2a9de4ae-f43a-4de2-8804-a525a477df53.gif">'],
-    [':bave:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/7c049de0-eec6-4e14-b8c7-80e566f3f171.gif">'],
-    [':malade:', '<img src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/7da97f26-9433-4cdd-9503-2a6efb2610fa.gif">']
+    [':)', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/326dda02-857a-41cd-b96b-10b670707d81.gif">'],
+    [':(', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a44b536e-2911-4fb3-a5a7-936772c03def.gif">'],
+    [':noel:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a4c82724-3ca7-471e-a1fe-1008213fd975.gif">'],
+    [':hap:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/0f68c004-092c-4aec-a006-b42252d21c16.gif">'],
+    [':coeur:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a7523aae-879c-4123-aa7d-ef8383e7a619.gif">'],
+    [':question:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/a7523aae-879c-4123-aa7d-ef8383e7a619.gif">'],
+    [':ok:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/48a51100-3155-45ec-b0c7-46b0ed19aea3.gif">'],
+    [':rire:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/2a9de4ae-f43a-4de2-8804-a525a477df53.gif">'],
+    [':bave:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/7c049de0-eec6-4e14-b8c7-80e566f3f171.gif">'],
+    [':malade:', '<img class="picture" src="https://pub-edb0c2460d754c5eb19e4f23b232496e.r2.dev/7da97f26-9433-4cdd-9503-2a6efb2610fa.gif">']
 ]
 
 // Petite vérif des doublons in the doubt
@@ -43,7 +43,4 @@ window.FormData = class extends FormData {
         }
     }
 }
-
-
-    })();
 
